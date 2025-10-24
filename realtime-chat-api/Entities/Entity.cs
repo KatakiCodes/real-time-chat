08be3242229c53d2399306f82ced25f7f376f1fe
Nelson Dos Santos <katakicode001@gmail.com>
2025-10-24 11:43:22 +0100

    fix: changes guid to int type for id

--- realtime-chat-api/Entities/Entity.cs
+++ realtime-chat-api/Entities/Entity.cs
@@ -4,13 +4,11 @@ namespace realtime_chat_api.Entities;
 
 public abstract class Entity
 {
-    public Guid Id { get; set; }
-    public Entity(Guid id)
+    public int Id { get; set; }
+    public Entity(int id)
     {
         Id = id;
     }
     public Entity()
-    {
-        Id = Guid.NewGuid();
-    }
+    {}
 }
