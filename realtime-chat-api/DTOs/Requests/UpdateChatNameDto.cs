09b586c1540fa1f8bf25e625e42de6ff04e20c17
Nelson Dos Santos <katakicode001@gmail.com>
2025-10-24 11:58:45 +0100

    fix: includes id field in update dtos

--- realtime-chat-api/DTOs/Requests/UpdateChatNameDto.cs
+++ realtime-chat-api/DTOs/Requests/UpdateChatNameDto.cs
@@ -5,5 +5,6 @@ namespace realtime_chat_api.DTOs.Requests;
 public record class UpdateChatNameDto
 {
     [Required]
+    public int id { get; init; }
     public string Name { get; init; } = string.Empty;
 }
--- realtime-chat-api/DTOs/Requests/UpdateMessageDto.cs
+++ realtime-chat-api/DTOs/Requests/UpdateMessageDto.cs
@@ -5,5 +5,6 @@ namespace realtime_chat_api.DTOs.Requests;
 public record class UpdateMessageDto
 {
     [Required]
+    public int id { get; init; }
     public string Content { get; init; } = string.Empty;
 }
