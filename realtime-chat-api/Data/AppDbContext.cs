7dbba27e3c228817d8cf9efbaf4df03495cea1f2
Nelson Dos Santos <katakicode001@gmail.com>
2025-10-24 18:46:23 +0100

    feat: creates and configure dbContext

---
+++ realtime-chat-api/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using realtime_chat_api.Entities;
+
+namespace realtime_chat_api.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Chat> Chats { get; set; }
+    public DbSet<User> Messages { get; set; }
+}
--- realtime-chat-api/Program.cs
+++ realtime-chat-api/Program.cs
@@ -1,8 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using realtime_chat_api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddOpenApi();
-
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
