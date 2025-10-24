23d5d5d1e1a0f475db139625a2bb13222a6331dd
Nelson Dos Santos <katakicode001@gmail.com>
2025-10-24 11:44:01 +0100

    fix: removing password field

--- realtime-chat-api/Entities/User.cs
+++ realtime-chat-api/Entities/User.cs
@@ -6,29 +6,25 @@ public class User : Entity
 {
     public string email { get; private set; }
     public string Username { get; private set; }
-    public string PasswordHash { get; private set; }
+
     public User()
     { }
 
-    public User(string email, string username, string passwordHash)
+    public User(string email, string username)
     {
         DomainException.When(string.IsNullOrEmpty(email), "Email cannot be empty");
         DomainException.When(string.IsNullOrEmpty(username), "Username cannot be empty");
-        DomainException.When(string.IsNullOrEmpty(passwordHash), "PasswordHash cannot be empty");
 
         this.email = email;
         this.Username = username;
-        this.PasswordHash = passwordHash;
     }
-    public User(Guid id, string email, string username, string passwordHash) : base(id)
+    public User(int id, string email, string username) : base(id)
     {
         DomainException.When(string.IsNullOrEmpty(email), "Email cannot be empty");
         DomainException.When(string.IsNullOrEmpty(username), "Username cannot be empty");
-        DomainException.When(string.IsNullOrEmpty(passwordHash), "PasswordHash cannot be empty");
         
         this.email = email;
         this.Username = username;
-        this.PasswordHash = passwordHash;
     }
     public void UpdateUserName(string username)
     {
