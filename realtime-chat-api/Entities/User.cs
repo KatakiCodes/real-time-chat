a7624d7562f906b9ef2c322c810a31667bfac20f
Nelson Dos Santos <katakicode001@gmail.com>
2025-10-31 10:43:27 +0100

    fix: Sets password field

--- realtime-chat-api/Entities/User.cs
+++ realtime-chat-api/Entities/User.cs
@@ -4,32 +4,37 @@ namespace realtime_chat_api.Entities;
 
 public class User : Entity
 {
-    public string email { get; private set; }
+    public string Email { get; private set; }
     public string Username { get; private set; }
+    public string Password { get; private set; }
     public List<Chat> AdministeredChats { get; private set; }
 
     public User()
     { }
 
-    public User(string email, string username)
+    public User(string email, string username, string password)
     {
         DomainException.When(string.IsNullOrEmpty(email), "Email cannot be empty");
         DomainException.When(string.IsNullOrEmpty(username), "Username cannot be empty");
+        DomainException.When(string.IsNullOrEmpty(password), "Password cannot be empty");
 
-        this.email = email;
+        this.Email = email;
         this.Username = username;
+        this.Password = password;
     }
-    public User(int id, string email, string username) : base(id)
+    public User(int id, string email, string username, string password) : base(id)
     {
         DomainException.When(string.IsNullOrEmpty(email), "Email cannot be empty");
         DomainException.When(string.IsNullOrEmpty(username), "Username cannot be empty");
+        DomainException.When(string.IsNullOrEmpty(password), "Password cannot be empty");
         
-        this.email = email;
+        this.Email = email;
         this.Username = username;
+        this.Password = password;
     }
     public void UpdateUserName(string username)
     {
-        DomainException.When(string.IsNullOrEmpty(email), "Username cannot be empty");
+        DomainException.When(string.IsNullOrEmpty(username), "Username cannot be empty");
         this.Username = username;
     }
 }
