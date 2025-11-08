3101668bf99dcdcdec7395fd64f182626c913046
Nelson Dos Santos <katakicode001@gmail.com>
2025-11-08 11:49:20 +0100

    feat: add validations at message service

--- realtime-chat-api/Services/MessageService.cs
+++ realtime-chat-api/Services/MessageService.cs
@@ -3,6 +3,7 @@ using AutoMapper;
 using realtime_chat_api.DomainExceptions;
 using realtime_chat_api.DTOs.Requests;
 using realtime_chat_api.DTOs.Responses;
+using realtime_chat_api.DTOs.Validations;
 using realtime_chat_api.Entities;
 using realtime_chat_api.Repositories.Interface;
 using realtime_chat_api.Services.Interface;
@@ -16,7 +17,7 @@ public class MessageService : IMessageService
     private IChatService _ChatService;
     private readonly IMapper _Mapper;
     private ResponseModel<MessageResponse> ResponseModel;
-    public MessageService(IMapper mapper,IUserService userService,IChatService chatService,IMessageRepository repository)
+    public MessageService(IMapper mapper, IUserService userService, IChatService chatService, IMessageRepository repository)
     {
         _Repository = repository;
         _UserService = userService;
@@ -27,30 +28,46 @@ public class MessageService : IMessageService
 
     public async Task<ResponseModel<MessageResponse>> CreateAsync(CreateMessageRequest request)
     {
-        ResponseModel<UserResponse?> findUser = await _UserService.GetByIdAsync(request.GetUserId());
-        ResponseModel<ChatResponse?> findChat = await _ChatService.GetByIdAsync(request.ChatId);
-        if (findUser.Data is null)
-            return ResponseModel.UNAUTHORIZED(["Invalid User."]);
-        if (findChat.Data is null)
-            return ResponseModel.NOTFOUND(["Chat not found."]);
-        Message message = _Mapper.Map<Message>(request);
-        message = await _Repository.CreateAsync(message);
-        return ResponseModel.CREATED(_Mapper.Map<MessageResponse>(message));
+        using (var validator = new CreateMessageRequestValidation())
+        {
+            var validationResult = validator.Validate(request);
+            if (!validationResult.IsValid)
+                return ResponseModel.BADREQUEST(validationResult.Errors.Select(e => e.ErrorMessage));
+
+            ResponseModel<UserResponse?> findUser = await _UserService.GetByIdAsync(request.GetUserId());
+            ResponseModel<ChatResponse?> findChat = await _ChatService.GetByIdAsync(request.ChatId);
+            if (findUser.Data is null)
+                return ResponseModel.UNAUTHORIZED(["Invalid User."]);
+            if (findChat.Data is null)
+                return ResponseModel.NOTFOUND(["Chat not found."]);
+            Message message = _Mapper.Map<Message>(request);
+            message = await _Repository.CreateAsync(message);
+            return ResponseModel.CREATED(_Mapper.Map<MessageResponse>(message));
+        }
     }
 
     public async Task<ResponseModel<MessageResponse>> EditMessageContentAsync(UpdateMessageRequest request)
     {
-        Message? findMessage = await _Repository.GetByIdAsync(request.Id);
-        if(findMessage is null)
-            return ResponseModel.NOTFOUND(["Message not found."]);
-        findMessage.UpdateContent(request.Content);
-        findMessage = await _Repository.UpdateAsync(findMessage);
-        return ResponseModel.OK(_Mapper.Map<MessageResponse>(findMessage));
+
+        using (var validator = new UpdateMessageRequestValidation())
+        {
+            var validationResult = validator.Validate(request);
+            if (!validationResult.IsValid)
+                return ResponseModel.BADREQUEST(validationResult.Errors.Select(e => e.ErrorMessage));
+
+            Message? findMessage = await _Repository.GetByIdAsync(request.Id);
+            if (findMessage is null)
+                return ResponseModel.NOTFOUND(["Message not found."]);
+            findMessage.UpdateContent(request.Content);
+            findMessage = await _Repository.UpdateAsync(findMessage);
+            return ResponseModel.OK(_Mapper.Map<MessageResponse>(findMessage));
+        }
+
     }
 
     public async Task<ResponseModel<IEnumerable<MessageResponse>>> GetByChatIdAsync(int chatId)
     {
-        ResponseModel<ChatResponse?> findChat =  await _ChatService.GetByIdAsync(chatId);
+        ResponseModel<ChatResponse?> findChat = await _ChatService.GetByIdAsync(chatId);
         if (findChat.Data is null)
             return new ResponseModel<IEnumerable<MessageResponse>>().NOTFOUND(["Chat not found."]);
         IEnumerable<Message> messages = await _Repository.GetMessagesByChatIdAsync(chatId);
