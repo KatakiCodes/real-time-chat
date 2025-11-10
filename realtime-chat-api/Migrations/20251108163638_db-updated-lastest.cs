47283f8b70d62b6c4544e096fc20dc4aecd282ec
Nelson Dos Santos <katakicode001@gmail.com>
2025-11-10 09:54:08 +0100

    config: set recent migrations

---
+++ realtime-chat-api/Migrations/20251108163638_db-updated-lastest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace realtime_chat_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class dbupdatedlastest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "email",
+                table: "Users",
+                newName: "Email");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "Email",
+                table: "Users",
+                newName: "email");
+        }
+    }
+}
