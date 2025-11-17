211563a1fe58234fee1c30b93ebfa9255e793666
Nelson Dos Santos <katakicode001@gmail.com>
2025-11-17 03:42:11 +0100

    feat: define domain rules for user_chats (about actibityState)

--- realtime-chat-api/Entities/User_Chat.cs
+++ realtime-chat-api/Entities/User_Chat.cs
@@ -8,6 +8,7 @@ namespace realtime_chat_api.Entities
         public User_Chat()
         {
             DateCreate = DateTime.UtcNow;
+            DefineInitialActivityState(isAdmin:false);
         }
         public User_Chat(int userId, int chatId, EUserChatActivityState activityState, bool isAdmin)
         {
@@ -16,7 +17,9 @@ namespace realtime_chat_api.Entities
             ActivityState = activityState;
             IsAdmin = isAdmin;
             DateCreate = DateTime.UtcNow;
+            DefineInitialActivityState(isAdmin);
         }
+        private EUserChatActivityState DefineInitialActivityState(bool isAdmin) => isAdmin ? EUserChatActivityState.CAN_INTERACT : EUserChatActivityState.WAIT_ACCESS;
 
         public User? User { get; private set; }
         public Chat? Chat { get; private set; }
@@ -25,5 +28,21 @@ namespace realtime_chat_api.Entities
         public EUserChatActivityState ActivityState { get; private set; }
         public bool IsAdmin { get; private set; }
         public DateTime DateCreate { get; private set; }
+
+        public void AllowInteract()
+        {
+            if(ActivityState != EUserChatActivityState.CAN_INTERACT)
+                ActivityState = EUserChatActivityState.CAN_INTERACT;
+        }
+        public void SetAsReaderOnly()
+        {
+            if(ActivityState != EUserChatActivityState.READ_ONLY)
+                ActivityState = EUserChatActivityState.READ_ONLY;
+        }
+        public void RemoveAccess()
+        {
+            if(ActivityState != EUserChatActivityState.WAIT_ACCESS)
+                ActivityState = EUserChatActivityState.WAIT_ACCESS;
+        }
     }
 }
